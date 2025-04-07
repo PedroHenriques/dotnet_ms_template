20ab2e8960ac095665b3c5cf091c90551a198e68
Pedro Henriques <pedro@pedrojhenriques.com>
2025-04-07 13:59:44 +0000

    Updates to scripts.

--- cli/test.sh
+++ cli/test.sh
@@ -7,6 +7,8 @@ FILTERS="";
 USE_DOCKER=0;
 RUNNING_IN_PIPELINE=0;
 RUN_LOCAL_ENV=0;
+TEST_TYPE="";
+COVERAGE="";
 
 while [ "$#" -gt 0 ]; do
   case "$1" in
@@ -14,16 +16,42 @@ while [ "$#" -gt 0 ]; do
     --docker) USE_DOCKER=1; shift 1;;
     --cicd) RUNNING_IN_PIPELINE=1; USE_DOCKER=1; shift 1;;
     --filter) FILTERS="--filter ${2}"; shift 2;;
-    --unit) FILTERS="--filter Type=Unit"; shift 1;;
-    --integration) FILTERS="--filter Type=Integration"; RUN_LOCAL_ENV=1; shift 1;;
-    --e2e) FILTERS="--filter Type=E2E"; RUN_LOCAL_ENV=1; shift 1;;
+    --unit) FILTERS="--filter Type=Unit"; TEST_TYPE="unit"; shift 1;;
+    --integration) FILTERS="--filter Type=Integration"; TEST_TYPE="integration"; RUN_LOCAL_ENV=1; shift 1;;
+    --e2e) FILTERS="--filter Type=E2E"; TEST_TYPE="e2e"; RUN_LOCAL_ENV=1; shift 1;;
+    --coverage) COVERAGE="--collect:\"XPlat Code Coverage\""; FILTERS="--filter Type=Unit"; TEST_TYPE="unit"; shift 1;;
 
     -*) echo "unknown option: $1" >&2; exit 1;;
     *) PROJ=$1; shift 1;;
   esac
 done
 
-CMD="dotnet test ${FILTERS} ${PROJ}";
+case "${TEST_TYPE}" in
+  "unit")
+    if [ ! -d "./test/unit/" ]; then
+      echo "No './test/unit/' directory found. Assuming no unit tests exist.";
+      exit 0;
+    fi
+    ;;
+  "integration")
+    if [ ! -d "./test/integration/" ]; then
+      echo "No './test/integration/' directory found. Assuming no integration tests exist.";
+      exit 0;
+    fi
+    ;;
+  "e2e")
+    if [ ! -d "./test/e2e/" ]; then
+      echo "No './test/e2e/' directory found. Assuming no e2e tests exist.";
+      exit 0;
+    fi
+    ;;
+esac
+
+if [ $RUN_LOCAL_ENV -eq 1 ]; then
+  sh ./cli/start.sh;
+fi
+
+CMD="dotnet test ${FILTERS} ${COVERAGE} ${PROJ}";
 
 if [ $WATCH -eq 1 ]; then
   if [ -z "$PROJ" ]; then
@@ -39,7 +67,7 @@ if [ $USE_DOCKER -eq 1 ]; then
     INTERACTIVE_FLAGS="-i";
   fi
 
-  docker run --rm ${INTERACTIVE_FLAGS} -v "./:/app/" -w "/app/" mcr.microsoft.com/dotnet/sdk:8.0-noble /bin/sh -c "$CMD";
+  docker run --rm ${INTERACTIVE_FLAGS} -v "./:/app/" -w "/app/" mcr.microsoft.com/dotnet/sdk:8.0-noble /bin/sh -c "${CMD}";
 else
-  $CMD;
+  eval "${CMD}";
 fi
